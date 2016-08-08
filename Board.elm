a7a3098975daa3771ea9e0432935b08cf4cbeaeb
Jeremie Jost <jeremiejost@gmail.com>
2016-08-08 11:40:27 -0400

    Base board template

---
+++ Board.elm
@@ -0,0 +1,23 @@
+import Html exposing (Html, div, table, tr, td, text)
+import Html.App as App
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
+import Light
+import List
+
+main =
+    App.beginnerProgram
+        { model = init
+        , update = update
+        , view = view
+        }
+
+
+-- MODEL
+
+
+-- UPDATE
+
+
+-- VIEW
+
